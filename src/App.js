9e34946475f1e0c740b8091e1c204eb62aa1a6af
Samika Sanghvi <sps76@pitt.edu>
2024-07-23 03:49:53 -0400

    Updated sidebar navigation and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-.App {
-  display: flex;
-  background-color: #1e1e1e;
-  height: 100vh;
-  color: white;
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Sidebar />
+            <MainContent />
+        </div>
+    );
 }
 
-body {
-  margin: 0;
-  font-family: 'Nanum Gothic', sans-serif;
-}
+export default App;
