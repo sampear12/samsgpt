28b8bab707665f77f8a0fb790a1228d04b1ac4ba
Samika Sanghvi <sps76@pitt.edu>
2024-08-11 17:57:04 +0530

    phone view

--- src/hooks/Typewriter.js
+++ src/hooks/Typewriter.js
@@ -4,16 +4,24 @@ const useTypewriter = (text, onComplete) => {
     const [displayedText, setDisplayedText] = useState('');
 
     useEffect(() => {
-        let currentIndex = 1;
+        let currentIndex = 0;
+        
+        // Detect if the user is on a mobile device based on screen width
+        const isMobile = window.innerWidth <= 600;
+        
+        // Set a faster speed for mobile devices
+        const speed = isMobile ? 4 : 4; // Adjust speed as needed, 50ms for mobile, 100ms for desktop
+
         const intervalId = setInterval(() => {
-            if (currentIndex < text.length-1) {
+            if (currentIndex < text.length) {
                 setDisplayedText((prev) => prev + text[currentIndex]);
                 currentIndex++;
             } else {
                 clearInterval(intervalId);
                 if (onComplete) onComplete();
             }
-        }, );
+        }, speed);
+
         return () => clearInterval(intervalId);
     }, [text, onComplete]);
 
